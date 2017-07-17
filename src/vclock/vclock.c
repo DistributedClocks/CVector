a660a1a6f64b8ce52b61a9e5bd2981bd48436f05
fruffy <fruffy@cs.ubc.ca>
2017-07-17 12:51:58 -0700

    Comment vclock

--- src/vclock/vclock.c
+++ src/vclock/vclock.c
@@ -1,16 +1,38 @@
+/**
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2017-2018 University of British Columbia
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ */
+
 #include "vclock.h"
 
 
-char * returnVCString(struct vectorClock *vc);
 int id_sort(struct vectorClock *a, struct vectorClock *b);
-struct vectorClock * sort_by_id(struct vectorClock *vc);
 
 
-//returns a new vector clock
 struct vectorClock * clockInit(char * c_id) {
     struct vectorClock *newClock = NULL;
     struct vectorClock *clock = (struct vectorClock*)malloc(sizeof(struct vectorClock));
-    strcpy(clock->id,  c_id);
+    strncpy(clock->id, c_id, VC_ID_LENGTH);
     clock->time = 0;
     HASH_ADD_STR(newClock, id, clock);
     return newClock;
@@ -22,23 +44,39 @@ void tick(struct vectorClock **vc, char * c_id) {
     HASH_FIND_STR(*vc, c_id, clock);  /* id already in the hash? */
     if (clock==NULL) {
         clock = (struct vectorClock*)malloc(sizeof(struct vectorClock));
-        strcpy(clock->id,  c_id);
+        strncpy(clock->id, c_id, VC_ID_LENGTH);
         clock->time = 0;
         HASH_ADD_STR(*vc, id, clock);  /* id: name of key field */
     }
     clock->time = clock->time + 1;
 }
+
 void set(struct vectorClock **vc, char * c_id, uint64_t ticks) {
     struct vectorClock *clock;
 
     HASH_FIND_STR(*vc, c_id, clock);  /* id already in the hash? */
     if (clock==NULL) {
         clock = (struct vectorClock*)malloc(sizeof(struct vectorClock));
-        strcpy(clock->id,  c_id);
+        strncpy(clock->id, c_id, VC_ID_LENGTH);
         HASH_ADD_STR(*vc, id, clock);  /* id: name of key field */
     }
     clock->time = ticks;
 }
+
+struct vectorClock * copy(struct vectorClock * vc) {
+    struct vectorClock *vc_copy= NULL;
+    struct vectorClock *clock;
+
+    for(clock=vc; clock != NULL; clock=(struct vectorClock*)(clock->hh.next)) {
+        struct vectorClock *tmp = (struct vectorClock*)malloc(sizeof(struct vectorClock));
+        strncpy(tmp->id,  clock->id, VC_ID_LENGTH);
+        tmp->time = clock->time;
+        HASH_ADD_STR(vc_copy, id, tmp);  // id: name of key field
+    }
+
+    return vc_copy;
+}
+
 uint64_t findTicks(struct vectorClock *vc, char * c_id) {
     struct vectorClock *clock;
     HASH_FIND_STR(vc, c_id, clock);
@@ -47,7 +85,8 @@ uint64_t findTicks(struct vectorClock *vc, char * c_id) {
     return clock->time;
 }
 
-uint64_t lastUpdate (struct vectorClock *vc, uint64_t last) {
+uint64_t lastUpdate (struct vectorClock *vc) {
+    uint64_t last = 0;
     struct vectorClock *clock;
     for(clock=vc; clock != NULL; clock=(struct vectorClock*)(clock->hh.next)) {
         if(clock->time > last)
@@ -64,7 +103,7 @@ void merge(struct vectorClock *vc, struct vectorClock *other) {
         HASH_FIND_STR(vc, o_clock->id, clock);  /* id already in the hash? */
         if (clock==NULL) {
             clock = (struct vectorClock*)malloc(sizeof(struct vectorClock));
-            strcpy(clock->id,  o_clock->id);
+            strncpy(clock->id,  o_clock->id, VC_ID_LENGTH);
             clock->time = o_clock->time;
             HASH_ADD_STR(vc, id, clock);  /* id: name of key field */
         } else {
@@ -74,33 +113,35 @@ void merge(struct vectorClock *vc, struct vectorClock *other) {
     }
 }
 
-struct vectorClock * copy(struct vectorClock * vc) {
-    struct vectorClock *vc_copy= NULL;
-    struct vectorClock *clock;
-
-    for(clock=vc; clock != NULL; clock=(struct vectorClock*)(clock->hh.next)) {
-        struct vectorClock *tmp = (struct vectorClock*)malloc(sizeof(struct vectorClock));
-        strcpy(tmp->id,  clock->id);
-        tmp->time = clock->time;
-        HASH_ADD_STR(vc_copy, id, tmp);  // id: name of key field
-    }
-
-    return vc_copy;
+int id_sort(struct vectorClock *a, struct vectorClock *b) {
+    return strcmp(a->id,b->id);
 }
 
-void printVC(struct vectorClock *vc) {
-    char * output = returnVCString(vc);
-    printf("%s\n",output);
-    free(output);
+int time_sort(struct vectorClock *a, struct vectorClock *b) {
+    return (a->time < b->time) ? -1 : (a->time > b->time);
 }
+
+void sort_by_id(struct vectorClock **vc) {
+    HASH_SORT(*vc, id_sort);
+}
+
+void sort_by_time(struct vectorClock **vc) {
+    HASH_SORT(*vc, time_sort);
+}
+
+struct vectorClock * copy_sort(struct vectorClock *vc) {
+    struct vectorClock *vc_sort = copy(vc);
+    HASH_SORT(vc_sort, id_sort);
+    return vc_sort;
+}
+
 /**TODO: Decide if we need sorting here.**/
-
 char * returnVCString(struct vectorClock *vc) {
     int num_clocks = HASH_COUNT(vc);
 
     char * vcString = malloc(num_clocks * (20 + 20 + 4) + 2);
     struct vectorClock *clock;
-/*    struct vectorClock * vc_sort = sort_by_id(vc);*/
+    //sort_by_id(&vc);
     int len = sprintf(vcString, "{");
     int i = 1;
     for(clock=vc; clock != NULL; clock=(struct vectorClock*)(clock->hh.next)) {
@@ -113,13 +154,10 @@ char * returnVCString(struct vectorClock *vc) {
     return vcString;
 }
 
-int id_sort(struct vectorClock *a, struct vectorClock *b) {
-    return (a->id < b->id) ? -1 : (a->id > b->id);
-}
-struct vectorClock * sort_by_id(struct vectorClock *vc) {
-    struct vectorClock *vc_sort = copy(vc);
-    HASH_SORT(vc_sort, id_sort);
-    return vc_sort;
+void printVC(struct vectorClock *vc) {
+    char * output = returnVCString(vc);
+    printf("%s\n",output);
+    free(output);
 }
 
 /**TODO: Figure out requirements for vector clock comparison.**/
--- src/vclock/vclock.h
+++ src/vclock/vclock.h
@@ -1,18 +1,135 @@
+/**
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2017-2018 University of British Columbia
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to 
+ * deal in the Software without restriction, including without limitation the 
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * DEALINGS IN THE SOFTWARE.
+ * 
+ */
+
 #include <stdio.h> //printf
 #include "uthash/uthash.h"
-#define VC_ID_LENGTH 30
-#define CLOCKSIZE VC_ID_LENGTH + 2 * 8
 
+/* The maximum size of a vector clock id. Can freely be modified. */
+#ifndef VC_ID_LENGTH
+#define VC_ID_LENGTH 30
+#endif
+
+/* The maximum size of a single vector clock. Depended on VC_ID_LENGTH.*/
+#ifndef CLOCKSIZE
+#define CLOCKSIZE VC_ID_LENGTH + 2 * 8
+#endif
+
+
+/**
+ * The vector clock map type. 
+ * "id" is a string of pre-specified fixed length.
+ * "time" is a 64 bit integer denoting the current time value of a clock.
+ * "hh" is a uthash extension which makes this data structure dynamic and 
+ * hashable.
+ */
 struct vectorClock {
 	char id[VC_ID_LENGTH];
 	uint64_t time;
-	UT_hash_handle hh;         /* makes this structure hashable */
+	UT_hash_handle hh;
 };
+
+/**
+ * Returns a new vector clock map initialised with the given process id.
+ * The time value of the vector clock structure contained is set to 0.
+ * The given char is the key of the clock, the time variable is the value.
+ * If the length of c_id exceeds VC_ID_LENGTH, the id name will be truncated.
+ * As hash map the uthash library is used, which allows for efficient and
+ * scalable manipulation of C.
+ */
 struct vectorClock *clockInit(char * c_id);
+
+/**
+ * Increments the time value of the vector clock "c_id" by one.
+ * If the specified id does not exist in the map, a new clock with a value of
+ * one will be created.
+ * To correctly increment the value of the vector map it is required to pass 
+ * the address of the vector map.
+ */
 void tick(struct vectorClock **vc, char * c_id);
+
+/**
+ * Sets the time value of the vector clock "c_id" to the value "ticks".
+ * If the specified id does not exist in the map, a new clock with a value of
+ * "ticks" will be created.
+ * To correctly increment the value of the vector map it is required to pass 
+ * the address of the vector map.
+ */
 void set(struct vectorClock **vc, char * c_id, uint64_t ticks);
+
+/**
+ * Returns a copy of the vector clock map. Both clock maps remain valid.
+ */
 struct vectorClock * copy(struct vectorClock * vc);
-void printVC (struct vectorClock *vc);
-char * returnVCString(struct vectorClock *vc);
+
+/**
+ * Returns the current time value of the clock "c_id". If this id does not 
+ * exist, a negative value is returned.
+ */
 uint64_t findTicks(struct vectorClock *vc, char * c_id);
+
+/**
+ * Returns the most recent update of all the clocks contained in the map.
+ * In this context, update means the current highest time value across all 
+ * clocks.
+ */
+uint64_t lastUpdate (struct vectorClock *vc);
+
+/**
+ * Merges the clock map "vc" with a second clock map "other". This operation 
+ * directly modifies "vc" and will result in "vc" encapsulating "other".
+ * If both maps contain the same specific id, the higher time value will be 
+ * chosen.
+ */
 void merge(struct vectorClock *vc, struct vectorClock *other);
+
+/**
+ * Sorts a clock map based on the lexical rank of the contained ids.
+ * This operation directly modifies the ordering of the vector clock.
+ */
+void sort_by_id(struct vectorClock **vc);
+
+/**
+ * Sorts a clock map based on the numerical rank of contained time values.
+ * This operation directly modifies the ordering of the vector clock.
+ */
+void sort_by_time(struct vectorClock **vc);
+
+/**
+ * Sorts a clock map based on the lexical rank of the contained ids.
+ * This operation returns a copy of the vector clock and does not modify the
+ * original.
+ */
+struct vectorClock * copy_sort(struct vectorClock *vc);
+
+/**
+ * Returns a string representation of the vector map in the following format:
+ * {"ProcessID 1": Time1, "ProcessID 2": Time2, ...}
+ */
+char * returnVCString(struct vectorClock *vc);
+
+/**
+ * Prints the string generated by returnVCString for a given vector map.
+ */
+void printVC (struct vectorClock *vc);
